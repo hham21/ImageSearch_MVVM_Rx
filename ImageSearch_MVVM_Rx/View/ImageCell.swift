9a605ffacc8bf721615b48d84cd9d21886b7b416
Hyoungsu Ham <hhyoungsu@gmail.com>
2020-06-03 11:11:27 +0900

    Add ImageCell

---
+++ ImageSearch_MVVM_Rx/View/ImageCell.swift
@@ -0,0 +1,60 @@
+//
+//  ImageCell.swift
+//  ImageSearch_MVVM_Rx
+//
+//  Created by Hyoungsu Ham on 2020/06/03.
+//  Copyright © 2020 Ham-Dev. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+import Kingfisher
+
+final class ImageCell: UICollectionViewCell {
+    static let identifier: String = .init(describing: ImageCell.self)
+    
+    let imageView: UIImageView = .init()
+    let favoriteButton: UIButton = .init()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setAttributes()
+        layoutViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setAttributes() {
+        imageView.do {
+            $0.contentMode = .scaleAspectFill
+            $0.layer.masksToBounds = true
+        }
+        
+        favoriteButton.do {
+            $0.setImage(UIImage(systemName: "suit.heart.fill"), for: .normal)
+            $0.tintColor = .red
+            $0.isHidden = true
+        }
+    }
+    
+    private func layoutViews() {
+        addSubview(imageView)
+        addSubview(favoriteButton)
+        
+        imageView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        favoriteButton.snp.makeConstraints {
+            $0.width.height.equalTo(50)
+            $0.top.right.equalToSuperview()
+        }
+    }
+    
+    func configure(with data: ImageCellData) {
+        imageView.kf.setImage(with: data.url)
+        favoriteButton.isHidden = !data.favorite
+    }
+}
--- ImageSearch_MVVM_Rx/View/SearchViewController.swift
+++ ImageSearch_MVVM_Rx/View/SearchViewController.swift
@@ -7,10 +7,28 @@
 //
 
 import UIKit
+import RxSwift
+import RxDataSources
 
-class SearchViewController: UIViewController {
+typealias DataSource = RxCollectionViewSectionedAnimatedDataSource<ImageSection>
 
+final class SearchViewController: UIViewController {
+    private var disposeBag: DisposeBag = .init()
+    
+    private let searchController: UISearchController = .init(searchResultsController: nil)
+    private let flowLayout: UICollectionViewFlowLayout = .init()
+    private lazy var collectionView: UICollectionView = .init(frame: view.bounds, collectionViewLayout: flowLayout)
+//    private lazy var dataSource: DataSource =
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    private func setAttribute() {
+        title = "이미지 검색"
+        view.backgroundColor = .white
+        navigationItem.searchController = searchController
+        
+        
     }
 }
